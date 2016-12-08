a71ba751b3fc564355a3aa503d2d200320c7bced
Echezona Nwankwo <echenwankwong@eonnauto.com>
2016-12-07 18:55:47 -0600

    Angular Controller for home & About Page

--- src/EonnAuto/wwwroot/ngApp/controllers/controllers.ts
+++ src/EonnAuto/wwwroot/ngApp/controllers/controllers.ts
@@ -1,9 +1,26 @@
 namespace EonnAuto.Controllers {
 
     export class HomeController {
-        public message = 'Hello from the home page!';
-    }
 
+        public vehicles;
+        public vehicleResource;
+        public vehicle;
+
+        public getVehicles() {
+            this.vehicles = this.vehicleResource.query();
+        }
+        public save() {
+            this.vehicleResource.save(this.vehicle).$promise.then(() => {
+                this.vehicle = null;
+                this.getVehicles();
+            });
+            
+        }
+        constructor(private $resource: angular.resource.IResourceService) {
+            this.vehicleResource = $resource('/api/vehicles/:id');
+            this.getVehicles();
+        }
+    }
 
     export class SecretController {
         public secrets;
@@ -14,10 +31,21 @@ namespace EonnAuto.Controllers {
             });
         }
     }
-
-
     export class AboutController {
         public message = 'Hello from the about page!';
+    }
+
+    export class DetailController {
+        public vehicle;
+
+        constructor(private $http: ng.IHttpService, private $stateParams:
+            ng.ui.IStateParamsService) {
+            $http.get(`/api/vehicles/${$stateParams['id']}`)
+                .then((response) => {
+                    this.vehicle = response.data;
+                    console.log(this.vehicle);
+                });
+        }
     }
 
 }
